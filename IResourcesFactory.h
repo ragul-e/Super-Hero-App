58f9b2bb0e945098a5fa5dc9f22a7281cd2dc4d5
ragul-e <83817912+ragul-e@users.noreply.github.com>
2021-09-09 21:31:54 +0530

    Update IResourcesFactory.h

--- IResourcesFactory.h
+++ IResourcesFactory.h
@@ -1,13 +1,1 @@
-#include <memory>
-#include <restbed>
 
-using namespace std;
-using namespace restbed;
-
-class IResourceFactory {
-
-public:
-
-    virtual shared_ptr<Resource> get_resource() const = 0;
-     
-};
